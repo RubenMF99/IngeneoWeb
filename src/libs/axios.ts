732ea3434330cc79b39a832dc3d73537e1c04471
RubenMF99 <rumarfo6@gmail.com>
2024-02-05 19:33:46 -0500

    Fix: Route fix

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -25,7 +25,7 @@ authApi.interceptors.response.use(
   (error) => {
     if (error.response && error.response.status === 401) {
       useAuthStore.getState().logout()
-      window.location.href = '/login'
+      window.location.href = '/#/login'
     }
     return Promise.reject(error)
   }
