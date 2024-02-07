b177268e159a0cc185c622a0c74cb5c590236a06
RubenMF99 <rumarfo6@gmail.com>
2024-02-06 19:30:27 -0500

    Fix: Delete product

--- src/components/products/DeleteProductModal.tsx
+++ src/components/products/DeleteProductModal.tsx
@@ -1,72 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ModalProps, Product } from '../../types/types'
-import { deleteProduct, getProduct } from '../../api/product'
+import { deleteProduct, getProducts } from '../../api/product'
 import swal from 'sweetalert'
 
 const DeleteProductModal = ({ setModal }: ModalProps) => {
-  const [search, setSearch] = useState('')
+  const [products, setProducts] = useState<null | Product[]>(null)
 
-  const [product, setProduct] = useState<null | Product>(null)
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const id = (e.currentTarget[0] as HTMLInputElement).value
-    try {
-      const response = await getProduct(id)
-      const { tiposProducto, transporte } = response.data
-
-      setProduct({
-        id: parseInt(id),
-        tiposProducto,
-        transporte,
-      })
-    } catch (err) {
-      console.error(err)
-      await swal('Error', 'No se pudo encontrar el almacen digitado', 'error')
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const resProducts = await getProducts()
+      setProducts(resProducts.data)
     }
-  }
+    fetchProducts()
+  }, [])
 
-  const handleDeleteProduct = async () => {
-    try {
-      if (product?.id) {
-        await deleteProduct(product.id)
-      }
-      setModal({ active: false, modal: '' })
-      await swal('Eliminado', 'Producto eliminado correctamente', 'success')
-    } catch (err) {
-      console.error(err)
-      await swal('Error', 'No se pudo eliminar el producto', 'error')
+  const handleDelete = async (id: number | undefined) => {
+    if (id) {
+      await deleteProduct(id)
+      swal('Producto eliminado', '', 'success')
+      setModal({
+        active: false,
+      })
     }
   }
 
   return (
     <>
       <h1 className="text-xl font-bold">Eliminar producto</h1>
-      <form className="flex flex-col mt-4" onSubmit={handleSubmit}>
-        <input
-          required
-          className="p-2 m-2 border-2 rounded-md"
-          type="number"
-          placeholder="Id del producto"
-          onChange={(e) => setSearch(e.target.value)}
-          value={search}
-        />
-        {product && (
-          <>
-            <div className="bg-green-200 p-2">
-              <p>ID: {product?.id}</p>
-              <p>Tipo: {product?.tiposProducto}</p>
-              <p>Transporte: {product?.transporte}</p>
+
+      {products && (
+        <>
+          {products.map((product) => (
+            <div key={product.id} className="bg-green-200 p-4 m-4">
+              <p>ID del producto: {product.id}</p>
+              <p>Tipo de Producto: {product.tiposProducto}</p>
+              <p>Transporte: {product.transporte}</p>
+              <button
+                className="bg-red-400 p-2 mt-2 rounded-md"
+                onClick={() => {
+                  handleDelete(product.id)
+                }}
+              >
+                Eliminar
+              </button>
             </div>
-            <input
-              onClick={handleDeleteProduct}
-              type="button"
-              className="p-2 m-2 bg-red-200 rounded-md"
-              value="Eliminar"
-            />
-          </>
-        )}
-      </form>
+          ))}
+        </>
+      )}
     </>
   )
 }
